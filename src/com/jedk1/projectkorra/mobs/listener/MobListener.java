3041b25d88ab48134489e507957d3eb6ebda4081
jedk1 <jedkristian@live.com>
2015-12-04 15:30:22 +0000

    Updated events

--- src/com/jedk1/projectkorra/mobs/listener/MobListener.java
+++ src/com/jedk1/projectkorra/mobs/listener/MobListener.java
@@ -1,19 +1,27 @@
 package com.jedk1.projectkorra.mobs.listener;
 
+import com.jedk1.projectkorra.mobs.MobMethods;
 import com.jedk1.projectkorra.mobs.ProjectKorraMobs;
 import com.jedk1.projectkorra.mobs.manager.EntityManager;
+import com.projectkorra.projectkorra.GeneralMethods;
 
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Zombie;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.entity.EntityTargetEvent;
+import org.bukkit.metadata.FixedMetadataValue;
 
 public class MobListener implements Listener {
 
 	ProjectKorraMobs plugin;
 
+	public static boolean airFallDamage = ProjectKorraMobs.plugin.getConfig().getBoolean("Properties.Air.NoFallDamage");
+	
 	public MobListener(ProjectKorraMobs plugin) {
 		this.plugin = plugin;
 	}
@@ -27,6 +35,34 @@ public class MobListener implements Listener {
 			LivingEntity entity = (LivingEntity) event.getEntity();
 			if (entity instanceof Zombie) {
 				EntityManager.addEntity(entity, (LivingEntity) event.getTarget());
+			}
+		}
+	}
+	
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void onMobSpawn(CreatureSpawnEvent event) {
+		LivingEntity entity = event.getEntity();
+		if (MobMethods.isDisabledWorld(entity.getWorld())) {
+			return;
+		}
+		if (entity instanceof Zombie) {
+			if (!entity.hasMetadata("element")) {
+				entity.setMetadata("element", new FixedMetadataValue(ProjectKorraMobs.plugin, GeneralMethods.rand.nextInt(4)));
+			}
+		}
+	}
+	
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void onEntityDamage(EntityDamageEvent event) {
+		if (event.getEntity() instanceof LivingEntity) {
+			LivingEntity entity = (LivingEntity) event.getEntity();
+			if (entity instanceof Zombie) {
+				if (entity.hasMetadata("element") && entity.getMetadata("element").size() > 0 && entity.getMetadata("element").get(0).asInt() == 0 && airFallDamage) {
+					if (event.getCause() == DamageCause.FALL) {
+						event.setCancelled(true);
+						return;
+					}
+				}
 			}
 		}
 	}
