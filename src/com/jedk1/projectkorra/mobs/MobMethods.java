20d3c34c14770e43c8027ef9f2c092a69e078551
jedk1 <jedkristian@live.com>
2015-12-04 15:31:51 +0000

    Added disabled worlds check

--- src/com/jedk1/projectkorra/mobs/MobMethods.java
+++ src/com/jedk1/projectkorra/mobs/MobMethods.java
@@ -1,12 +1,18 @@
 package com.jedk1.projectkorra.mobs;
 
+import com.projectkorra.projectkorra.ProjectKorra;
 import com.projectkorra.projectkorra.earthbending.EarthMethods;
 
+import org.bukkit.World;
 import org.bukkit.block.Block;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class MobMethods {
+	
+	public static List<String> disabledWorlds = new ArrayList<String>();
 
 	@SuppressWarnings("deprecation")
 	public static boolean isTransparent(Block block) {
@@ -14,5 +20,21 @@ public class MobMethods {
 			return false;
 		}
 		return true;
+	}
+	
+	public static boolean isDisabledWorld(World world) {
+		if (disabledWorlds.contains(world.getName())) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static void registerDisabledWorlds() {
+		disabledWorlds.clear();
+		if (ProjectKorra.plugin.getConfig().getStringList("Properties.DisabledWorlds") != null) {
+			for (String s : ProjectKorra.plugin.getConfig().getStringList("Properties.DisabledWorlds")) {
+				disabledWorlds.add(s);
+			}
+		}
 	}
 }
