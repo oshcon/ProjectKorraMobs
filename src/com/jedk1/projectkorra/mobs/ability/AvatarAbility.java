95e60a19b06ec644a7d7fee53197c3329cfc5f0c
jedk1 <jedkristian@live.com>
2015-12-08 15:42:25 +0000

    Change to use AvatarAbility

---
+++ src/com/jedk1/projectkorra/mobs/ability/AvatarAbility.java
@@ -0,0 +1,29 @@
+package com.jedk1.projectkorra.mobs.ability;
+
+import com.jedk1.projectkorra.mobs.ability.air.AirAbility;
+import com.jedk1.projectkorra.mobs.ability.earth.EarthAbility;
+import com.jedk1.projectkorra.mobs.ability.fire.FireAbility;
+import com.jedk1.projectkorra.mobs.ability.water.WaterAbility;
+import com.projectkorra.projectkorra.GeneralMethods;
+
+import org.bukkit.entity.LivingEntity;
+
+public class AvatarAbility {
+	
+	public static void execute(LivingEntity entity, LivingEntity target) {
+		switch (GeneralMethods.rand.nextInt(4)) {
+			case 0:
+				AirAbility.execute(entity, target);
+				break;
+			case 1:
+				EarthAbility.execute(entity, target);
+				break;
+			case 2:
+				FireAbility.execute(entity, target);
+				break;
+			case 3:
+				WaterAbility.execute(entity, target);
+				break;
+		}
+	}
+}
